37a3e7be4c07bb2009bd74c1460e4c2706383600
deivid0067 <deivid3453@outlook.com>
2024-05-19 20:04:39 -0300

    Finalizando as telas de login e cadastro

---
+++ js/forms.js
@@ -0,0 +1,21 @@
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login_button')
+
+const handleFocus = (e) => {
+    const span = e.target.previousElementSibling;
+    span.classList.add("active")
+}
+
+const handleFocusOut = (e) => {
+    if (e.target.value === '') {
+        const span = e.target.previousElementSibling;
+        span.classList.remove("active")
+    }
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener('focus', handleFocus);
+    input.addEventListener('blur', handleFocusOut);
+});
+
